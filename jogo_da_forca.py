b3804455e3c4844dfa280f8f5db034ef0646d8ee
Diego Rodrigues <diegorodriguesb@hotmail.com>
2022-05-06 15:50:51 -0300

    Adicionando formatação de não aceitar caractere especial no input do usuário

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -61,7 +61,7 @@ def sorteio_palavra():
 # Essa função formata o input dos chutes para não receber caractere especial, mais de 1 carectere ou números.
 def formata_chute_input():
     chute = input("Qual letra? ").strip().upper()
-    if len(chute) != 1 or re.match("\d",chute):
+    if len(chute) != 1 or re.match("\d",chute) or re.match("\W",chute):
         return 0
     else:
         return chute
